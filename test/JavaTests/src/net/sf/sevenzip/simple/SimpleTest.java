477e0a8c6168c06988da1fbffe363fd783d29e34
Boris Brodski <boris_brodski@users.sourceforge.net>
2009-05-25 16:03:05 +0000

    JUnit added

---
+++ test/JavaTests/src/net/sf/sevenzip/simple/SimpleTest.java
@@ -0,0 +1,18 @@
+package net.sf.sevenzip.simple;
+
+import org.junit.Test;
+
+/**
+ * 
+ * 
+ * 
+ * @author boris
+ *
+ */
+public class SimpleTest {
+	
+	@Test
+	public void test() {
+		System.out.println("Test!!!!!!!!!!");
+	}
+}
