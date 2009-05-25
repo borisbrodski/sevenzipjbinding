f0ffaeaa205db15d8123cd7e15b8d186eaed90aa
Boris Brodski <boris_brodski@users.sourceforge.net>
2009-05-25 17:21:30 +0000

    Test data for java junit tests

---
+++ test/JavaTests/testdata/simple/compress.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+rm zip/*.zip
+for i in 0 5 9
+do
+    for j in *.dat
+    do
+	zip -u -$i zip/$j.$i.zip $j
+    done
+done
+
+rm 7z/*.7z
+for i in 0 5 9
+do
+    for j in *.dat
+    do
+	7z a -t7z -m0=lzma -mx=$i 7z/$j.$i.7z $j
+    done
+done
+
+rm rar/*.rar
+for i in 0 2 5
+do
+    for j in *.dat
+    do
+	rar a -m$i rar/$j.$i.rar $j
+    done
+done
+
+rm tar/*.tar
+for i in 0
+do
+    for j in *.dat
+    do
+	tar cf tar/$j.$i.tar $j
+    done
+done
+
+rm gzip/*.gz
+for i in 1 5 9
+do
+    for j in *.dat
+    do
+	cat $j | gzip -$i > gzip/$j.$i.gz
+    done
+done
+
+rm bzip2/*.bz2
+for i in 1 5 9
+do
+    for j in *.dat
+    do
+	cat $j | bzip2 -$i > bzip2/$j.$i.bz2
+    done
+done
