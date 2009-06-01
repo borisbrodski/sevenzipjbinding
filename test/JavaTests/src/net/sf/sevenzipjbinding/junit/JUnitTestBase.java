2291908914e388c0882f61ac7390b7bddac165d2
Boris Brodski <boris_brodski@users.sourceforge.net>
2009-06-01 09:29:12 +0000

    Renaming packages: sevenzip=>sevenzipjbinding

---
+++ test/JavaTests/src/net/sf/sevenzipjbinding/junit/JUnitTestBase.java
@@ -0,0 +1,26 @@
+package net.sf.sevenzipjbinding.junit;
+
+import net.sf.sevenzipjbinding.SevenZip;
+import net.sf.sevenzipjbinding.SevenZipNativeInitializationException;
+
+import org.junit.BeforeClass;
+
+/**
+ * This is the base class for all JUnit test classes. This class provides:<br>
+ * - Initialization of the native library
+ * 
+ * @author boris
+ * 
+ */
+public class JUnitTestBase {
+	/**
+	 * Initialize native SevenZipJBinding library for all JUnit tests
+	 * 
+	 * @throws SevenZipNativeInitializationException
+	 *             in case initialization of SevenZipJBinding fails
+	 */
+	@BeforeClass
+	public static void initializeSevenZipJBinding() throws SevenZipNativeInitializationException {
+		SevenZip.initSevenZipFromPlatformJAR();
+	}
+}
