2eaa7bd691808a07a7a63449b881ac84ae805fe7
Boris Brodski <boris_brodski@users.sourceforge.net>
2009-07-30 22:27:29 +0000

    CMake files: get compiled on Win32

--- cmake/FindMinGWm10-DLL.cmake
+++ cmake/FindMinGWm10-DLL.cmake
@@ -1,23 +1,26 @@
-IF(WIN32 OR WIN64)
+IF(CMAKE_HOST_WIN32) #true for win64 also
     MESSAGE("-- Looking for mingwm10.dll")
-    IF (CMAKE_SYSTEM_NAME MATCHES "CYGWIN.*")
-        # CygWin
-        FIND_FILE(MINWGM10_DLL_FILENAME "mingwm10.dll" $ENV{PATH})
-    ELSE(CMAKE_SYSTEM_NAME MATCHES "CYGWIN.*")
-        # MinGW
-        FIND_FILE(MINWGM10_DLL_FILENAME "mingwm10.dll" $ENV{PATH})
-    ENDIF(CMAKE_SYSTEM_NAME MATCHES "CYGWIN.*")
+    GET_FILENAME_COMPONENT(COMPILER_BIN_DIR "${CMAKE_CXX_COMPILER}" PATH)
+
+    FIND_FILE(MINWGM10_DLL_FILENAME mingwm10.dll "$ENV{PATH}" "${COMPILER_BIN_DIR}")
     MARK_AS_ADVANCED(MINWGM10_DLL_FILENAME)
 
-    IF (CMAKE_SYSTEM_NAME MATCHES "CYGWIN.*")
+    IF(MINWGM10_DLL_FILENAME)
+        MESSAGE("-- Looking for mingwm10.dll - found: ${MINWGM10_DLL_FILENAME}")
+    ELSE()
+        MESSAGE(FATAL_ERROR "mingwm10.dll not found. Please point cmake to mingwm10.dll using GUI or -DMINWGM10_DLL_FILENAME=<path-to-dll> option.")
+    ENDIF()
+
+    IF (CYGWIN)
         # CygWin
         FIND_PROGRAM(CYGPATH_EXE_FILENAME "cygpath.exe" $ENV{PATH})
-        MARK_AS_ADVANCED(CYGPATH_EXE_FILENAME)
-    ENDIF(CMAKE_SYSTEM_NAME MATCHES "CYGWIN.*")
+        IF(CYGPATH_EXE_FILENAME)
+            MESSAGE("-- Looking for cygpath.exe - found: ${CYGPATH_EXE_FILENAME}")
+        ELSE()
+            MESSAGE(FATAL_ERROR "cygpath.exe not found. Please point cmake to mingwm10.dll using GUI or -DCYGPATH_EXE_FILENAME=<path-to-exe> option.")
+        ENDIF()
 
-    IF(CYGPATH_EXE_FILENAME)
-        MESSAGE("-- Looking for mingwm10.dll - found: ${CYGPATH_EXE_FILENAME}")
-    ELSE()
-        MESSAGE(FATAL_ERROR "mingwm10.dll not found. Please point cmake to mingwm10.dll using GUI or -DCYGPATH_EXE_FILENAME=<path-to-dll> option.")
+        MARK_AS_ADVANCED(CYGPATH_EXE_FILENAME)
     ENDIF()
-ENDIF(WIN32 OR WIN64)
+
+ENDIF(CMAKE_HOST_WIN32)
