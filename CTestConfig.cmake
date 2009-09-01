76bcb6f21622ca7319396f75faefa9dffd693fc0
Boris Brodski <boris_brodski@users.sourceforge.net>
2009-09-01 18:58:40 +0000

    CTest configuration for CDash

---
+++ CTestConfig.cmake
@@ -0,0 +1,17 @@
+## This file should be placed in the root directory of your project.
+## Then modify the CMakeLists.txt file in the root directory of your
+## project to incorporate the testing dashboard.
+## # The following are required to uses Dart and the Cdash dashboard
+##   ENABLE_TESTING()
+##   INCLUDE(Dart)
+set(CTEST_PROJECT_NAME "kdelibs")
+set(CTEST_NIGHTLY_START_TIME "20:00:00 CET")
+
+set(CTEST_DROP_METHOD "http")
+set(CTEST_DROP_SITE "my.cdash.org")
+set(CTEST_DROP_LOCATION "/submit.php?project=7-Zip-JBinding")
+set(CTEST_DROP_SITE_CDASH TRUE)
+
+set(CTEST_CUSTOM_MAXIMUM_NUMBER_OF_WARNINGS 10000)
+#set(CTEST_CUSTOM_WARNING_EXCEPTION "kdecore/network/k3socket[a-z]+\\.h"
+#                                   "kdecore/network/k3clientsocketbase\\.h" )
