f3cb9afaf3d996d7583c08a928f7f4a9ebbf4d09
Boris Brodski <boris_brodski@users.sourceforge.net>
2009-11-30 13:24:09 +0100

    New files for multi-part rar archive snippet (copied from 7z ones)

---
+++ test/JavaTests/src/net/sf/sevenzipjbinding/junit/snippets/OpenMultipartArchive7zTest.java
@@ -0,0 +1,40 @@
+package net.sf.sevenzipjbinding.junit.snippets;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+
+import org.junit.Test;
+
+/**
+ * Tests {@link OpenMultipartArchive7z} snippet
+ * 
+ * @author Boris Brodski
+ * @version 4.65-1
+ */
+public class OpenMultipartArchive7zTest extends SnippetTest {
+	private String getExpectedOutput() {
+		/* BEGIN_OUTPUT(OpenMultipartArchiveOutput) */
+		String expected = "   Size   | Compr.Sz. | Filename\n";
+		expected += "----------+-----------+---------\n";
+		expected += "        6 |       114 | folder/file in folder.txt\n";
+		expected += "     4481 |      null | file1.txt\n";
+		expected += "       75 |      null | file2.txt\n";
+		expected += "        0 |         0 | folder\n";
+		/* END_OUTPUT */
+
+		expected = expected.replace("\n", System.getProperty("line.separator"));
+		expected = expected.replace('/', File.separatorChar);
+		return expected;
+	}
+
+	@Test
+	public void testListItemsInArchiveStandard() {
+		String expected = getExpectedOutput();
+
+		beginSnippetTest();
+		OpenMultipartArchive7z.main(new String[] { "testdata/snippets/multipart-7z.7z.001" });
+		String output = endSnippetTest();
+		assertEquals(expected, output);
+	}
+}
