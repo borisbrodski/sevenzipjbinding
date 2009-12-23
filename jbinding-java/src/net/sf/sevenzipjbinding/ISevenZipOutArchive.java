b72c54d879d2e4277bb99294905f4a841fcb8577
Boris Brodski <boris_brodski@users.sourceforge.net>
2009-12-23 14:44:23 +0100

    ISevenZipOutArchive with a single method updateItems(...)

---
+++ jbinding-java/src/net/sf/sevenzipjbinding/ISevenZipOutArchive.java
@@ -0,0 +1,32 @@
+package net.sf.sevenzipjbinding;
+
+/**
+ * The interface provides functionality to create a new archive or update an existing archive.<br>
+ * <br>
+ * Standard way to get implementation is to use {@link SevenZip}.<br>
+ * <br>
+ * The last call should be a call of the method {@link ISevenZipInArchive#close()}. After this call no more Methods
+ * should be called.
+ * 
+ * @author Boris Brodski
+ * @version 9.04-2.0
+ * 
+ */
+public interface ISevenZipOutArchive {
+
+    /**
+     * Create/update items in archive. If {@link ISequentialOutStream} was created stand alone via {@link SevenZip}
+     * class, only new archive creation is possible. To update an existing archive open it first and then use
+     * {@link ISevenZipInArchive} to get a {@link ISevenZipOutArchive} implementation.
+     * 
+     * @param outStream
+     *            output stream to get the new archive
+     * @param numberOfItems
+     *            number of items in the new archive
+     * @param archiveUpdateCallback
+     *            update call back object to provide more information for archive create/update operation.
+     */
+    public void updateItems(ISequentialOutStream outStream, int numberOfItems,
+            IArchiveUpdateCallback archiveUpdateCallback);
+
+}
