729149d22015dc5f1cf9c95de2d09d58eb9f7dfa
Boris Brodski <boris_brodski@users.sourceforge.net>
2010-01-06 23:10:02 +0100

    Javadoc: Minor bugfix in ISequentialInStream

--- jbinding-java/src/net/sf/sevenzipjbinding/ISequentialInStream.java
+++ jbinding-java/src/net/sf/sevenzipjbinding/ISequentialInStream.java
@@ -8,8 +8,8 @@ package net.sf.sevenzipjbinding;
  */
 public interface ISequentialInStream {
     /**
-     * Reads at least 1 and maximum <code>data.length</code> from the in-stream. If <code>data.length == 0</code> 0
-     * should be returned. If <code>data.length != 0</code>, then return value 0 indicates end-of-stream (EOF). This
+     * Reads at least 1 and maximum <code>data.length</code> bytes from the in-stream. If <code>data.length == 0</code>
+     * 0 should be returned. If <code>data.length != 0</code>, then return value 0 indicates end-of-stream (EOF). This
      * means no more bytes can be read from the stream. <br>
      * This function is allowed to read less than number of remaining bytes in stream and less then
      * <code>data.length</code>. You must call <code>read()</code> function in loop, if you need exact amount of data.
