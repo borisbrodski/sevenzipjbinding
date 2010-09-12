350218e36f10dee23e23ef353878b92f74e368cb
Boris Brodski <boris_brodski@users.sourceforge.net>
2010-09-12 19:08:01 +0200

    Ignore ArchiveFormatTest. This test should be either completely rewritten or removed.

--- test/JavaTests/src/net/sf/sevenzipjbinding/junit/ArchiveFormatTest.java
+++ test/JavaTests/src/net/sf/sevenzipjbinding/junit/ArchiveFormatTest.java
@@ -4,20 +4,21 @@ import junit.framework.Assert;
 import net.sf.sevenzipjbinding.ArchiveFormat;
 import net.sf.sevenzipjbinding.SevenZip;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class ArchiveFormatTest extends JUnitNativeTestBase {
     @Test
+    @Ignore
+    // TODO Check, this test. The outArchive should be at least closed afterwards.
     public void testForOutArchive() {
         for (ArchiveFormat archiveFormat : ArchiveFormat.values()) {
             try {
                 SevenZip.openOutArchive(archiveFormat);
-                Assert
-                        .assertTrue("Problem with archive format " + archiveFormat, archiveFormat
-                                .isOutArchiveSupported());
+                Assert.assertTrue("Problem with archive format " + archiveFormat, archiveFormat.isOutArchiveSupported());
             } catch (Exception e) {
-                Assert.assertFalse("Problem with archive format " + archiveFormat, archiveFormat
-                        .isOutArchiveSupported());
+                Assert.assertFalse("Problem with archive format " + archiveFormat,
+                        archiveFormat.isOutArchiveSupported());
             }
         }
     }
