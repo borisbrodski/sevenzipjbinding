cd40a80a8f8938fd3926fcda24ef017af3e7c600
Boris Brodski <boris_brodski@users.sourceforge.net>
2010-09-12 19:00:43 +0200

    Bugfix: Make JObjectMap synchronized

--- jbinding-cpp/JObjectList.h
+++ jbinding-cpp/JObjectList.h
@@ -10,24 +10,33 @@
 
 #include <list>
 #include "jni.h"
+#include "BaseSystem.h"
 
+
+/**
+ * Synchronized object map
+ */
 template<typename JOBJ, typename VALUE>
 class JObjectMap {
     struct Item {
         JOBJ jobj;
         VALUE value;
     };
+    PlatformCriticalSection _criticalSection;
     std::list<Item> jobjectList;
     typedef typename std::list<Item>::iterator iterator;
 public:
     VALUE & add(JOBJ const & jobj) {
+        _criticalSection.Enter();
         jobjectList.push_front(Item());
         Item & item = *jobjectList.begin();
         item.jobj = jobj;
+        _criticalSection.Leave();
         return item.value;
     }
 
     VALUE * get(JNIEnv * env, JOBJ const & jobj) {
+        _criticalSection.Enter();
         iterator iter = jobjectList.begin();
         for (; iter != jobjectList.end(); iter++) {
             TRACE("MAP: Checking " << iter->jobj);
@@ -39,13 +48,17 @@ public:
                     jobjectList.splice(jobjectList.begin(), jobjectList, iter);
                 }
                 // Put element on top
-                return &(iter->value);
+                VALUE * result = &(iter->value);
+                _criticalSection.Leave();
+                return result;
             }
         }
         TRACE("MAP: Not found");
 
+        _criticalSection.Leave();
         return NULL;
     }
+
     int size() {
         return jobjectList.size();
     }
