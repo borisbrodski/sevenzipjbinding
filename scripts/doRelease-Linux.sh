2c84ead602b78e7137548b519d85e86e47dd26d3
linux32 <boris@linux32-VirtualBox.(none)>
2012-01-03 22:30:37 +0100

    Scripts (doDebug & doRelease) bug fixes

--- scripts/doRelease-Linux.sh
+++ scripts/doRelease-Linux.sh
@@ -1,10 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 rm -rf SevenZipJBinding.Release/
 mkdir SevenZipJBinding.Release/
 cd SevenZipJBinding.Release/
 cmake ../SevenZipJBinding -DCMAKE_BUILD_TYPE=Release -DJAVA_JDK=/usr/lib/jvm/java-5-sun
 make
 ctest -D Experimental
+make package
 read -p "Upload to the sourceforge web space (y/n)? "
 if [ "$REPLY" == "y" ] ; then
     cmake -P ../SevenZipJBinding/scripts/upload-release.cmake
