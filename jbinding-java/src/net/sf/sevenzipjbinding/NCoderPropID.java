5fb45076549089bc4ba4a78c98f08c54c0da75a2
Boris Brodski <boris_brodski@users.sourceforge.net>
2012-02-23 08:49:40 +0100

    Add NCoderPropID

---
+++ jbinding-java/src/net/sf/sevenzipjbinding/NCoderPropID.java
@@ -0,0 +1,33 @@
+package net.sf.sevenzipjbinding;
+
+/**
+ * Enumeration for possible NCoder properties
+ * 
+ * @author Boris Brodski
+ * @version 9.13-2.00
+ */
+public enum NCoderPropID {
+    kBlockSize(int.class, "C"), //
+    kDictionarySize(int.class, "D"), //
+    kUsedMemorySize(int.class, "MEM"), //
+    kOrder(int.class, "O"), //
+    kPosStateBits(int.class, "PB"), //
+    kLitContextBits(int.class, "LC"), //
+    kLitPosBits(int.class, "LP"), //
+    kEndMarker(boolean.class, "eos"), //
+    kNumPasses(int.class, "Pass"), //
+    kNumFastBytes(int.class, "fb"), //
+    kMatchFinderCycles(int.class, "mc"), //
+    kAlgorithm(int.class, "a"), //
+    kMatchFinder(String.class, "mf"), //
+    kNumThreads(int.class, "mt"), //
+    kDefaultProp(int.class, "");
+
+    private final Class<?> clazz;
+    private final String option;
+
+    private NCoderPropID(Class<?> clazz, String option) {
+        this.clazz = clazz;
+        this.option = option;
+    }
+}
