b9cef60e93c9d87968e95006a6e87be137f3f2c7
Boris Brodski <boris_brodski@users.sourceforge.net>
2013-12-07 23:30:03 +0100

    Fix missing user32.dll related stuff on Windows

--- jbinding-cpp/PlatformMinGW/StdAfx.h
+++ jbinding-cpp/PlatformMinGW/StdAfx.h
@@ -1,7 +1,9 @@
 #ifndef __STDAFX_H__INCLUDED__
 #define __STDAFX_H__INCLUDED__
 
-#include <windows.h>
-#include <time.h>
+#ifndef NO_MY_STDAFX
+  #include <windows.h>
+  #include <time.h>
+#endif // NO_MY_STDAFX
 
 #endif // __STDAFX_H__INCLUDED__
