52edb32f358a6cbc3ba5d66c9a2761b5da597a15
Boris Brodski <boris_brodski@users.sourceforge.net>
2015-09-28 08:23:00 +0200

    Win: update build scripts

--- scripts/Windows/env-win.bat
+++ scripts/Windows/env-win.bat
@@ -3,4 +3,5 @@
 REM SET HTTP_PROXY=proxy.server:8080
 
 SET MSYS_CMD=C:\WINDOWS\SysWOW64\cmd.exe /c "C:\Program Files (x86)\Git\bin\sh.exe" --login -c
-SET SCP_UPLOAD_PATH=sevenzipjbinding@xtextcasts.org:/home/sevenzipjbinding/release/
+REM SET SCP_UPLOAD_PATH=sevenzipjbinding@xtextcasts.org:/home/sevenzipjbinding/release/
+SET SCP_UPLOAD_PATH=boris_brodski,sevenzipjbind@frs.sourceforge.net:/home/frs/project/s/se/sevenzipjbind/OldFiles/
