6856762bb01b0e71ec92e90b6791378ae59fda58
Boris Brodski <brodsky_boris@yahoo.com>
2015-10-07 22:35:18 +0200

    Maven: add test project connected to the snapshot repo

---
+++ test/sevenzipjbinding-maven-test/src/main/java/net/sf/sevenzipjbinding/maven/test/SimpleTest.java
@@ -0,0 +1,13 @@
+package net.sf.sevenzipjbinding.maven.test;
+
+import net.sf.sevenzipjbinding.SevenZip;
+import net.sf.sevenzipjbinding.SevenZipNativeInitializationException;
+
+import org.junit.Test;
+
+public class SimpleTest {
+	@Test
+	public void initTest() throws SevenZipNativeInitializationException {
+		SevenZip.initSevenZipFromPlatformJAR();
+	}
+}
