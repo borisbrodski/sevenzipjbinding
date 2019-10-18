a45d5da05f43b660b5d4ef59caff18b2cabb24d4
Boris Brodski <brodsky_boris@yahoo.com>
2019-10-18 21:07:21 +0200

    Text: Remove InnerClasses from Test-Code due to java bug
    Unfortunately, inner classes within the test method hit the java bug on
    ARM (armel and armhf):
    
    https://bugs.java.com/bugdatabase/view_bug.do?bug_id=2210448
    
    The exception
    
    java.lang.IncompatibleClassChangeError:
    net.sf.sevenzipjbinding.junit.tools.TypeVariableResolverTest and
    net.sf.sevenzipjbinding.junit.tools.TypeVariableResolverTest$7B disagree
    on InnerClasses attribute
    
    occurs, if java source code (JUnit test) is compiled on x86 and
    then run on armel or armhf.

--- test/JavaTests/src/net/sf/sevenzipjbinding/junit/tools/TypeVariableResolverTest.java
+++ test/JavaTests/src/net/sf/sevenzipjbinding/junit/tools/TypeVariableResolverTest.java
@@ -11,164 +11,216 @@ import net.sf.sevenzipjbinding.junit.tools.TypeVariableResolver.TypeVariableReso
 public final class TypeVariableResolverTest {
     // TODO Add support for interfaces
 
-    @Test
-    public void testSimple() throws TypeVariableResolverException {
+    // Unfortunately, inner classes within the test method hit the java bug on ARM (armel and armhf).
+    //   https://bugs.java.com/bugdatabase/view_bug.do?bug_id=2210448
+    interface TestSimple {
         class A<T> {
         }
+
         class B extends A<String> {
         }
+    }
 
-        Class<?> result = resolveTypeVariable(B.class, A.class, "T");
+    @Test
+    public void testSimple() throws TypeVariableResolverException {
+        // Unfortunately, inner classes within the test method hit the java bug on ARM (armel and armhf).
+        //   https://bugs.java.com/bugdatabase/view_bug.do?bug_id=2210448
+        //
+        // class A<T> {
+        // }
+        // class B extends A<String> {
+        // }
+        Class<?> result = resolveTypeVariable(TestSimple.B.class, TestSimple.A.class, "T");
         assertEquals(String.class, result);
+    }
+
+    interface TestVarNotDefined {
+        class A<T> {
+        }
+
+        class B extends A<String> {
+        }
     }
 
     @Test
     public void testVarNotDefined() {
-        class A<T> {
-        }
-        class B extends A<String> {
-        }
         try {
-            resolveTypeVariable(B.class, A.class, "A");
+            resolveTypeVariable(TestVarNotDefined.B.class, TestVarNotDefined.A.class, "A");
             fail("Exception expected");
         } catch (TypeVariableResolverException e) {
-            assertEquals(
-                    "Type variable 'A' is not defined in the " + A.class.getName() + " class",
+            assertEquals("Type variable 'A' is not defined in the " + TestVarNotDefined.A.class.getName() + " class",
                     e.getMessage());
+        }
+    }
+
+    interface TestClassDoesntExtend {
+        class A<T> {
         }
     }
 
     @Test
     public void testClassDoesntExtend() {
-        class A<T> {
-        }
         try {
-            resolveTypeVariable(String.class, A.class, "T");
+            resolveTypeVariable(String.class, TestClassDoesntExtend.A.class, "T");
             fail("Exception expected");
         } catch (TypeVariableResolverException e) {
-            assertEquals(
-                    "The concreteClass (java.lang.String) should extend the definingClass (" + A.class.getName() + ")",
-                    e.getMessage());
+            assertEquals("The concreteClass (java.lang.String) should extend the definingClass ("
+                    + TestClassDoesntExtend.A.class.getName() + ")", e.getMessage());
+        }
+    }
+
+    interface TestSameClass {
+        class A<T> {
         }
     }
 
     @Test
     public void testSameClass() {
-        class A<T> {
-        }
         try {
-            resolveTypeVariable(A.class, A.class, "T");
+            resolveTypeVariable(TestSameClass.A.class, TestSameClass.A.class, "T");
             fail("Exception expected");
         } catch (TypeVariableResolverException e) {
-            assertEquals("The concreteClass (" + A.class.getName() + ") should extend the definingClass ("
-                    + A.class.getName() + ")", e.getMessage());
+            assertEquals("The concreteClass (" + TestSameClass.A.class.getName() + ") should extend the definingClass ("
+                    + TestSameClass.A.class.getName() + ")", e.getMessage());
+        }
+    }
+
+    interface TestNonGenericClasses {
+        class A {
+        }
+
+        class B extends A {
         }
     }
 
     @Test
     public void testNonGenericClasses() {
-        class A {
-        }
-        class B extends A {
-        }
         try {
-            resolveTypeVariable(B.class, A.class, "T");
+            resolveTypeVariable(TestNonGenericClasses.B.class, TestNonGenericClasses.A.class, "T");
             fail("Exception expected");
         } catch (TypeVariableResolverException e) {
-            assertEquals("Type variable 'T' is not defined in the " + A.class.getName() + " class", e.getMessage());
+            assertEquals("Type variable 'T' is not defined in the " + //
+                    TestNonGenericClasses.A.class.getName() + " class", e.getMessage());
+        }
+    }
+
+    interface TestVariableWithoutValue {
+        class A<T> {
+        }
+
+        class B<T> extends A<T> {
         }
     }
 
     @Test
     public void testVariableWithoutValue() {
-        class A<T> {
-        }
-        class B<T> extends A<T> {
-        }
         try {
-            resolveTypeVariable(B.class, A.class, "T");
+            resolveTypeVariable(TestVariableWithoutValue.B.class, TestVariableWithoutValue.A.class, "T");
             fail("Exception expected");
         } catch (TypeVariableResolverException e) {
             assertEquals("Can't trace the type variable to the concrete type", e.getMessage());
         }
     }
 
-    @Test
-    public void testNonGenericChildClass() throws TypeVariableResolverException {
+    interface TestNonGenericChildClass {
         class A<T> {
         }
+
         class B extends A<String> {
         }
+
         class C extends B {
         }
+    }
 
-        Class<?> result = resolveTypeVariable(C.class, A.class, "T");
+    @Test
+    public void testNonGenericChildClass() throws TypeVariableResolverException {
+        Class<?> result = resolveTypeVariable(TestNonGenericChildClass.C.class, TestNonGenericChildClass.A.class, "T");
         assertEquals(String.class, result);
+    }
+
+    interface TestTwoTypeVarsInTwoLevelHierarchy {
+        class A<T1> {
+        }
+
+        class B<T2> extends A<String> {
+        }
+
+        class C extends B<Integer> {
+        }
     }
 
     @Test
     public void testTwoTypeVarsInTwoLevelHierarchy() throws TypeVariableResolverException {
+        Class<?> result = resolveTypeVariable(TestTwoTypeVarsInTwoLevelHierarchy.C.class,
+                TestTwoTypeVarsInTwoLevelHierarchy.A.class, "T1");
+        assertEquals(String.class, result);
+    }
+
+    interface TestTwoTypeVarsInTwoLevelHierarchy2 {
         class A<T1> {
         }
-        class B<T2> extends A<String> {
-        }
-        class C extends B<Integer> {
+
+        class B<T2, T3> extends A<T3> {
         }
 
-        Class<?> result = resolveTypeVariable(C.class, A.class, "T1");
-        assertEquals(String.class, result);
+        class C extends B<Integer, String> {
+        }
     }
 
     @Test
     public void testTwoTypeVarsInTwoLevelHierarchy2() throws TypeVariableResolverException {
-        class A<T1> {
-        }
-        class B<T2, T3> extends A<T3> {
-        }
-        class C extends B<Integer, String> {
+        Class<?> result = resolveTypeVariable(TestTwoTypeVarsInTwoLevelHierarchy2.C.class,
+                TestTwoTypeVarsInTwoLevelHierarchy2.A.class, "T1");
+        assertEquals(String.class, result);
+    }
+
+    interface TestComplex {
+        class A<T1, T2, T3> {
         }
 
-        Class<?> result = resolveTypeVariable(C.class, A.class, "T1");
-        assertEquals(String.class, result);
+        class B<T2, T3, T4> extends A<T3, T4, T2> {
+        }
+
+        class C<T5, T6> extends B<T6, String, T5> {
+        }
+
+        class D<T1> extends C<Integer, T1> {
+        }
+
+        class E extends D<Long> {
+        }
+
+        class F extends E {
+        }
+
+        class G<T1, T2, T3, T4, T5, T6> extends F {
+        }
+
+        class H extends G<Byte, Character, Float, Double, Boolean, Short> {
+        }
     }
 
     @Test
     public void testComplex() throws TypeVariableResolverException {
-        class A<T1, T2, T3> {
-        }
-        class B<T2, T3, T4> extends A<T3, T4, T2> {
-        }
-        class C<T5, T6> extends B<T6, String, T5> {
-        }
-        class D<T1> extends C<Integer, T1> {
-        }
-        class E extends D<Long> {
-        }
-        class F extends E {
-        }
-        class G<T1, T2, T3, T4, T5, T6> extends F {
-        }
-        class H extends G<Byte, Character, Float, Double, Boolean, Short> {
-        }
+        assertEquals(String.class, resolveTypeVariable(TestComplex.H.class, TestComplex.A.class, "T1"));
+        assertEquals(Integer.class, resolveTypeVariable(TestComplex.H.class, TestComplex.A.class, "T2"));
+        assertEquals(Long.class, resolveTypeVariable(TestComplex.H.class, TestComplex.A.class, "T3"));
 
-        assertEquals(String.class, resolveTypeVariable(H.class, A.class, "T1"));
-        assertEquals(Integer.class, resolveTypeVariable(H.class, A.class, "T2"));
-        assertEquals(Long.class, resolveTypeVariable(H.class, A.class, "T3"));
+        assertEquals(Long.class, resolveTypeVariable(TestComplex.H.class, TestComplex.B.class, "T2"));
+        assertEquals(String.class, resolveTypeVariable(TestComplex.H.class, TestComplex.B.class, "T3"));
+        assertEquals(Integer.class, resolveTypeVariable(TestComplex.H.class, TestComplex.B.class, "T4"));
 
-        assertEquals(Long.class, resolveTypeVariable(H.class, B.class, "T2"));
-        assertEquals(String.class, resolveTypeVariable(H.class, B.class, "T3"));
-        assertEquals(Integer.class, resolveTypeVariable(H.class, B.class, "T4"));
+        assertEquals(Integer.class, resolveTypeVariable(TestComplex.H.class, TestComplex.C.class, "T5"));
+        assertEquals(Long.class, resolveTypeVariable(TestComplex.H.class, TestComplex.C.class, "T6"));
 
-        assertEquals(Integer.class, resolveTypeVariable(H.class, C.class, "T5"));
-        assertEquals(Long.class, resolveTypeVariable(H.class, C.class, "T6"));
+        assertEquals(Long.class, resolveTypeVariable(TestComplex.H.class, TestComplex.D.class, "T1"));
 
-        assertEquals(Long.class, resolveTypeVariable(H.class, D.class, "T1"));
-
-        assertEquals(Byte.class, resolveTypeVariable(H.class, G.class, "T1"));
-        assertEquals(Character.class, resolveTypeVariable(H.class, G.class, "T2"));
-        assertEquals(Float.class, resolveTypeVariable(H.class, G.class, "T3"));
-        assertEquals(Double.class, resolveTypeVariable(H.class, G.class, "T4"));
-        assertEquals(Boolean.class, resolveTypeVariable(H.class, G.class, "T5"));
-        assertEquals(Short.class, resolveTypeVariable(H.class, G.class, "T6"));
+        assertEquals(Byte.class, resolveTypeVariable(TestComplex.H.class, TestComplex.G.class, "T1"));
+        assertEquals(Character.class, resolveTypeVariable(TestComplex.H.class, TestComplex.G.class, "T2"));
+        assertEquals(Float.class, resolveTypeVariable(TestComplex.H.class, TestComplex.G.class, "T3"));
+        assertEquals(Double.class, resolveTypeVariable(TestComplex.H.class, TestComplex.G.class, "T4"));
+        assertEquals(Boolean.class, resolveTypeVariable(TestComplex.H.class, TestComplex.G.class, "T5"));
+        assertEquals(Short.class, resolveTypeVariable(TestComplex.H.class, TestComplex.G.class, "T6"));
     }
 }
