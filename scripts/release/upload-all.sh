9c1047a418d6e5a83bbd520c215c274d66e9aa67
Boris Brodski <brodsky_boris@yahoo.com>
2020-02-19 09:22:18 +0100

    Update release scripts

--- scripts/release/upload-all.sh
+++ scripts/release/upload-all.sh
@@ -5,7 +5,15 @@ if [[ "$*" == "" ]] ; then
   exit 1
 fi
 
+echo -n "Enter version (e.g. 9.20-2.00beta) "
+read VERSION
+
+TARGET="boris_brodski,sevenzipjbind@frs.sourceforge.net:/home/frs/project/s/se/sevenzipjbind/7-Zip-JBinding/$VERSION/"
+
+echo -n "Uploading to $TARGET. Proceed?"
+read
+
 for file in "$@" ; do
   #scp "$file" "boris_brodski,sevenzipjbind@frs.sourceforge.net:/home/frs/project/s/se/sevenzipjbind/OldFiles/"
-  scp "$file" "boris_brodski,sevenzipjbind@frs.sourceforge.net:/home/frs/project/s/se/sevenzipjbind/7-Zip-JBinding/9.20-2.00beta/"
+  scp "$file" "$TARGET"
 done
